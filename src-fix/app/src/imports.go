587e9fcfa603ccaa1d74369e719713733c2c1cca
Andreas Heissenberger <andreas@heissenberger.at>
2021-05-06 11:02:30 +0000

    update

---
+++ src-fix/app/src/imports.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+	_ "github.com/project-flogo/aws-contrib/trigger/lambda"
+	_ "github.com/project-flogo/flow"
+	_ "github.com/project-flogo/contrib/activity/actreturn"
+	_ "github.com/project-flogo/contrib/function/string"
+)
